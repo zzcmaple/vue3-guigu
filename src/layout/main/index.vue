32eacf640d41521f540c985f00e2cb60a96c2442
zzcmaple <liulian778ha@163.com>
2023-06-25 19:07:15 +0800

    feat: tabbar搭建完成

---
+++ src/layout/main/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view #="{ Component }">
+    <Transition name="fade">
+      <component :is="Component"></component>
+    </Transition>
+  </router-view>
+</template>
+
+<style scoped lang="scss">
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 0.3s ease-in-out;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+</style>
---
+++ src/views/test/afterSales/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="work-order">
+    <h1>AfterSales</h1>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
